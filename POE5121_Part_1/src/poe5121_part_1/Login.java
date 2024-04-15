bb0202337e2cc92940685c01ca96beea9024d25b
MahiraGovender_ST10438866 <166383770+MahiraGovender@users.noreply.github.com>
2024-04-15 21:32:09 +0200

    POE_Part_1

--- POE5121_Part_1/src/poe5121_part_1/Login.java
+++ POE5121_Part_1/src/poe5121_part_1/Login.java
@@ -4,51 +4,64 @@ package poe5121_part_1;
 import javax.swing.*;
 
 public class Login {
+       //My fields for password and username
+       private String userName;
+       private String password;
+        
+       //Constant declared for special characters
+       private static final String SPECIAL_CHARACTERS = "!@#$%^&*()-+?_=,<>/";
     
-    private String userName;
-    private String password;
-    private String firstName;
-    private String lastName;
-    
-    
-    private static final String SPECIAL_CHARACTERS = "!@#$%^&*()-+?_=,<>/";
-    
-    public Login (String userName, String password, String firstName, String lastName) {
-       this.userName = userName;
-       this.password = password;
-       this.firstName = firstName;
-       this.lastName = lastName;
+       /*Create my constructer, must match the class's name with parameters to capture and bring 
+       username, password, firstName and lastName that will be inputed earlier after prompts in main*/
+       public Login (String userName, String password, String firstName, String lastName) {
+        
+       this.userName = userName; //This used because username will be tested in method 1
+       this.password = password; //This used because password will be tested in method 2
+       
     }
-    
-    public boolean checkUserName (String userName) {
+       /*First method: boolean type to check that the user has inputted a correctly formatted username for
+       registration*/
+       public boolean checkUserName (String userName) {
+        //Brings user name entered in main here to be tested
         this.userName = userName;     
         
+        //If statement is redundent but this is the only way all conditions are tested and a loop can occur for false
         if (!userName.isBlank() && userName.matches(".*[_].*") && userName.length() <= 5) {
             return true;
+            /*the application will only allow usernames that:
+            are not blank, have an underscore and is 5 characters or less in length*/
         }
-        else {
+        else { //or else it is considered incorrectly formatted
             
            return false;
            }
         }
-    
+      //Second method: boolean type to check that the user's password meets the set criteria
       public boolean checkPasswordComplexity(String password) {
         this.password = password;
         
+        //If statement is redundent but this is the only way all conditions are tested and a loop can occur for false
         if (!password.isBlank() && password.length() >= 8 &&
                 password.matches(".*[A-Z].*") &&
                 password.matches(".*[0-9].*") &&
                 password.matches(".*[" + SPECIAL_CHARACTERS + "].*"))
+        //Constant used here to check for special characters
         {
             return true;
+            /*The application only allows passwords that:
+            are not blank, have a capital letter, have a number and have a special character*/
         }
-        else {
+        else { //or else it is considered incorrectly formatted
             return false;
         }
         
       }
-   
-   public boolean registerUser () {
+       
+        /* Third method: boolean type checks what both method 1 & 2 are returning before giving an appropriate
+        response with regards to a user's registration status*/
+        public boolean registerUser () {
+            
+            //Put methods 1 & 2 in If statement to displaying registration failed when one or both are false
         if (!checkUserName(userName) || !checkPasswordComplexity(password)) {
             JOptionPane.showMessageDialog(null, "Registration failed. Please ensure that the username and password meet the requirements.");
             return true;
@@ -57,31 +70,47 @@ public class Login {
             return false;
         }
    }
-   
-   public boolean loginUser () {
-       //Re-entering data for login verification
+        
+        /*Fourth method: boolean type is for the user to login, checks that the user's inputs for username
+        and password match what they previously entered for their registration*/
+       public boolean loginUser () {
+           
+       //Prompts user to capture their input in order to proceed with testing
        String LoginuserName = JOptionPane.showInputDialog(null, "Please enter your user name to login");
        String Loginpassword = JOptionPane.showInputDialog(null, "Please enter your password to login");
        
+        /*If statement checks that they both match what was previously entered before the application
+        can proceed to the next instruction*/
         if (LoginuserName.matches(userName) && Loginpassword.matches(password)) {
         return true;
+        //Signal to proceed
        } 
         
-        else {
+        else { //Alerts user that they made a mistake 
             JOptionPane.showMessageDialog(null, "Invalid username or password. Please try again.");
         return false;
        }
-        
+        //This method will be implicated in method 5 not main for simultainious execution
    }
-   
-    public String returnLoginStatus() {
+       
+        /*Fifth method: String type is a message to tell the user if they managed to login or not
+        even after method 4 shows the message for incorrect usernames or passwords*/
+    public String returnLoginStatus(String firstName, String lastName) {
+         
+         //Implimenting my 3rd method here for a simultaneous execution in main 
         if (loginUser()) {
-            String success = "Successfull login! \nWelcome " + firstName + ",  " + lastName + " it is great to see you.\"";
+            String success = "Successfull login! \nWelcome " + firstName + ", " + lastName + " it is great to see you.\"";
             return success;
-        } else {
+            /*Returns this message to be displayed via a JOptionPane.showMessageDialog box 
+            that will be implimented in main if loginUser is true*/
+        } 
+        else {
             String fail ="Failed login";
             return fail;
+            /*Returns this message to be displayed via a JOptionPane.showMessageDialog box 
+            that will be implimented in main if loginUser is false*/
         }
+         //This method will be implicated in main within a JOptionPane.showMessageDialog box.
     }
     
   }
--- POE5121_Part_1/src/poe5121_part_1/POE5121_Part_1.java
+++ POE5121_Part_1/src/poe5121_part_1/POE5121_Part_1.java
@@ -6,107 +6,126 @@ import javax.swing.*; //* as a wild card
 public class POE5121_Part_1 {
 
     public static void main(String[] args) {
-        Login login = new Login("", "", "", "");
+        Login login = new Login("", "", "", ""); //Implments my Login class
         
         JOptionPane.showMessageDialog(null, "Welcome to my website"); //introduction to new user
        
        //input user's name       
        String firstName =  JOptionPane.showInputDialog(null, "Please enter your First Name to register");
        
-       //Name is required field with a set format
+       //Referencing code on how to do a do while loop with user input
+    
+    /************************************************************
+    *********************************************************
+    Title: JOptionPane.showInputDialog loop (using do while loop)
+    Author: asiew, StackOverflow
+    Date: 2015
+    Code version: 2.0
+    Availability: http:www.overflow.com/questions/343943359/
+    joptionpane-showinputdialog-loop-using-do-while-loop
+    *********************************************************
+    *************************************************************/
+       
+         //Name is required field with a set format
          do {
            if (firstName.isBlank() || firstName.matches(".*[0-9].*") || 
               firstName.matches(".*[  '!' + '@' + '#' + '$' + '%' + '^' + '&' + '*' + '(' + ')' + '-' + '_' + '+' + '=' + '{' + '}' + '[' + ']' + ';' + ':' + '<' + '>' + ',' + '.' + '?' + '/' + '|'].*")) {
                
+               //First name must not be blank nor have a number or a special character in it
            firstName = JOptionPane.showInputDialog(null, "Please enter your First Name to register");
            }
            
            else {
                  
-           break;}
+           break;} //Discontinues loop
            
          }while (firstName.isBlank() || firstName.matches(".*[0-9].*") ||
                    firstName.matches(".*[  '!' + '@' + '#' + '$' + '%' + '^' + '&' + '*' + '(' + ')' + '-' + '_' + '+' + '=' + '{' + '}' + '[' + ']' + ';' + ':' + '<' + '>' + ',' + '.' + '?' + '/' + '|'].*"));
+         //Loop for incorrect firstNames
               
        //Input user's surname       
         String lastName =  JOptionPane.showInputDialog(null, "Please enter your Last Name to register");
         
-       //Surname is required field with a set format
-       do {
+         //Surname is required field with a set format
+         do {
               if (lastName.isBlank() || lastName.matches(".*[0-9].*")||
                  lastName.matches(".*[  '!' + '@' + '#' + '$' + '%' + '^' + '&' + '*' + '(' + ')' + '-' + '_' + '+' + '=' + '{' + '}' + '[' + ']' + ';' + ':' + '<' + '>' + ',' + '.' + '?' + '/' + '|'].*")) {
-           
+                 
+                  //Last name must not be blank nor have a number or a special character in it
                  lastName = JOptionPane.showInputDialog(null, "Please enter your Last Name to register");
               }
               
               else {
                   
-                  break;
+                  break; //Discontinues loop
               }
            
          }while (lastName.isBlank() || lastName.matches(".*[0-9].*")||
                    lastName.matches(".*[  '!' + '@' + '#' + '$' + '%' + '^' + '&' + '*' + '(' + ')' + '-' + '_' + '+' + '=' + '{' + '}' + '[' + ']' + ';' + ':' + '<' + '>' + ',' + '.' + '?' + '/' + '|'].*"));
-      
+                   //Loop for incorrect firstNames
         
-        
-                 
-       //Input new userName
-          
-          String userName = JOptionPane.showInputDialog(null, """
+        //Input new userName
+        String userName = JOptionPane.showInputDialog(null, """
                 Please create a user Name to register.
                 (Your User Name must be no longer than 5 characters long and must contain an underscore)""");
         
         if (!login.checkUserName(userName)) {
        
-        //the loop for username format
-            do {
+        //Loop for incorrectly formatted usernames
+            do { 
+                
                  JOptionPane.showMessageDialog(null, 
                 "Username is not correctly formatted, please ensure that your "
                 + "username contains an underscore and is no longer than 5 characters in length");
                  
+                 //Displays registration failed only
                   login.registerUser();
                   
+                  //Re-promptes user to recapture username
                   userName =  JOptionPane.showInputDialog(null, """
                 Please create a user Name to register.
                 (Your User Name must be no longer than 5 characters long and must contain an underscore)""");
-               }while (!login.checkUserName(userName));
+               }while (!login.checkUserName(userName)); //Exits with a correctly formatted username
             
           }
           
-            //Input new password
-            String password =  JOptionPane.showInputDialog(null,  """
-            Please create a password to register.
-            (Your password must be 8 or more characters long,
-            must contain a capital letter, a number and a special character)""");
+        //Input new password
+        String password =  JOptionPane.showInputDialog(null,  """
+        Please create a password to register.
+        (Your password must be 8 or more characters long,
+        must contain a capital letter, a number and a special character)""");
             
         if (!login.checkPasswordComplexity(password)) {
               
-           //Loop response for password format
+           //Loop response for incorrectly formatted passwords
            do {  
                
                  JOptionPane.showMessageDialog(null, """
                   Password is not correctly formatted, please ensure that the 
                   password contains at least 8 characters, a capital letter, a number and a special character""");
                  
+                 //Displays registration failed only
                  login.registerUser();
                  
+                 //Re-promptes user to recapture password
                  password =  JOptionPane.showInputDialog(null,  """
                Please create a password to register.
                (Your password must be 8 or more characters long,
                must contain a capital letter, a number and a special character)""");
                  
-             }while (!login.checkPasswordComplexity(password));
+             }while (!login.checkPasswordComplexity(password)); //Exits with a correctly formatted password
          }
         //Correct password format response
          else {
              JOptionPane.showMessageDialog(null, "Password successfully captured");
            }
         
-        
+        //Response to correctly formatted usernames and passwords
+        //Only displays registration successful 
         login.registerUser();
      
-
-        JOptionPane.showMessageDialog(null, login.returnLoginStatus());
+        //Simultaneous execution of method 4 and 5 to display login success at the end in JOptionPane.showMessageDialog box
+        JOptionPane.showMessageDialog(null, login.returnLoginStatus(firstName, lastName));
 
     }
     
